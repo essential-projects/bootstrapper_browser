d50a6a427d3f3f618a31e14f09fff6f6261e96b7
Paul <paul.heidenreich@5minds.de>
2017-11-10 17:09:08 +0100

    :sparkles: Initial commit

---
+++ src/app_bootstrapper.ts
@@ -0,0 +1,39 @@
+import {ExtensionBootstrapper} from '@essential-projects/bootstrapper';
+import {ExtensionDiscoveryTag as extensionDiscoveryTag} from '@essential-projects/core_contracts';
+import {Container, IFactoryAsync, IInstanceWrapper} from 'addict-ioc';
+import {ConfigResolver} from './config_resolver';
+
+export class AppBootstrapper {
+
+  private _container: Container<IInstanceWrapper<any>>;
+  private extensionBootstrapperFactory: IFactoryAsync<ExtensionBootstrapper>;
+  private extensionBootstrapper: ExtensionBootstrapper;
+  private processEngineConfiguration: any;
+
+  constructor(_container: Container<IInstanceWrapper<any>>,
+              extensionBootstrapperFactory: IFactoryAsync<ExtensionBootstrapper>,
+              processEngineConfiguration: any) {
+    this._container = _container;
+    this.extensionBootstrapperFactory = extensionBootstrapperFactory;
+    this.processEngineConfiguration = processEngineConfiguration;
+  }
+
+  protected get container(): Container<IInstanceWrapper<any>> {
+    return this._container;
+  }
+
+  private initializeConfigProvider(): void {
+    this.container.settings.resolver = new ConfigResolver(this.processEngineConfiguration);
+  }
+
+  public async initialize(): Promise<void> {
+    this.extensionBootstrapper = await this.extensionBootstrapperFactory([extensionDiscoveryTag]);
+
+    this.initializeConfigProvider();
+  }
+
+  public async start(): Promise<void> {
+    await this.extensionBootstrapper.start();
+  }
+
+}
